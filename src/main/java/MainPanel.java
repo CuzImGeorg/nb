e4040a0451a0d551f954dbd0840109bb4a97ca16
SamiOsele <Samiosele23@gmail.com>
2022-03-17 14:34:16 +0100

    siu

--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -10,12 +10,39 @@ public class MainPanel extends JPanel {
         JMenu menu= new JMenu("session");
         JMenuItem item= new JMenuItem("Add Player to session");
         item.addActionListener(l -> {
-                JTextField TextPane1= new JTextField();
-                add(TextPane1);
-                TextPane1.setLocation(500,500);
-                TextPane1.setBorder(new LineBorder(Color.BLACK, 100));
-                TextPane1.setVisible(true);
-                updateUI();
+            JTextArea username= new JTextArea();
+            username.setBorder(new LineBorder(Color.BLACK,2));
+            username.setBackground(Color.white);
+            username.setBounds(722, 80,100,30);
+            username.setText("Username");
+            add(username);
+            setSize(100,25);
+            setLayout(null);
+            username.setEditable(true);
+            JTextArea password= new JTextArea();
+            password.setBorder(new LineBorder(Color.BLACK,2));
+            password.setBackground(Color.white);
+            password.setBounds(722, 120,100,30);
+            password.setText("Password");
+            add(password);
+            setSize(100,25);
+            setLayout(null);
+            password.setEditable(true);
+            updateUI();
+
+            JButton btn= new JButton();
+
+            btn.setBorder(new LineBorder(Color.BLACK,2));
+            btn.setBackground(Color.GREEN);
+            btn.setBounds(722, 160,100,30);
+            btn.setVisible(true);
+            btn.setText("Submit");
+            add(btn);
+            setSize(100,25);
+            setLayout(null);
+            updateUI();
+            btn.addActionListener(e -> Start.getSession().addSpieler(username.getText(),password.getText()));
+
         });
 
 
