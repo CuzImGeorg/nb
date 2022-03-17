da033d0182bf3920046a1efa64ad2bff459d0b4e
SamiOsele <Samiosele23@gmail.com>
2022-03-17 18:28:22 +0100

    siu

--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -1,13 +1,11 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
 
 public class MainPanel extends JPanel {
     JMenuBar menubar;
+    private ArrayList<Spieler>  spieler= new ArrayList<>();
 
     MainPanel(){
 
@@ -52,19 +50,70 @@ public class MainPanel extends JPanel {
                 remove(password);
                 remove(btn);
                 updateUI();
-                currentUser();
+                StartBtnUeberpruefung();
             });
 
+
+        });
+        JMenuItem item2 = new JMenuItem("Remove Player");
+        item.addActionListener(l -> {
+
+           for (int i = 0; i < spieler.size(); i++) {
+
+               JButton BtnRemove= new JButton();
+               BtnRemove.setBorder(new LineBorder(Color.BLACK,2));
+               BtnRemove.setBackground(Color.GREEN);
+               BtnRemove.setBounds(800, 80 + (i*20),100,30);
+               BtnRemove.setVisible(true);
+               BtnRemove.setText("Remove");
+               add(BtnRemove);
+               setSize(100,25);
+               setLayout(null);
+
+            }
 
         });
 
 
 
+
+
         menu.add(item);
+        menu.add(item2);
         menubar.add(menu);
         add(menubar);
         loggout();
         currentUser();
+    }
+    public void StartBtnUeberpruefung(){
+        if(Start.getSession().getLoggedInspieler().isEmpty()){
+
+        }else{
+            spieler= Start.getSession().getLoggedInspieler();
+            if(spieler.size() >= 2){
+                JButton SpielStart = new JButton();
+                SpielStart.setBorder(new LineBorder(Color.BLACK,2));
+                SpielStart.setBackground(Color.GREEN);
+                SpielStart.setBounds(722, 750,100,30);
+                SpielStart.setVisible(true);
+                SpielStart.setText("Starte Spiel");
+                add(SpielStart);
+                setSize(100,25);
+                setLayout(null);
+                updateUI();
+                SpielStart.addActionListener(e -> {
+                    removeAll();
+                    revalidate();
+                    repaint();
+
+
+                    SpielFeld();
+                });
+            }
+        }
+    }
+    public void SpielFeld(){
+
     }
     public void loggout() {
         JMenu logout = new JMenu("System");
@@ -76,22 +125,14 @@ public class MainPanel extends JPanel {
     }
 
     public void currentUser() {
-
-            Start.getSession().getLoggedInspieler().forEach(Spieler::toStringd);
-            if(!Start.getSession().getLoggedInspieler().isEmpty()) {
-                for (Spieler s : Start.getSession().getLoggedInspieler()) {
-
-                    JTextArea t = new JTextArea();
-                    t.setText(s.getUsername());
-                    t.setBorder(new LineBorder(Color.black, 2));
-                    t.setBounds(20, Start.getSession().getLoggedInspieler().indexOf(s) * 40,100,20);
-                    t.setVisible(true);
-                    t.setEditable(false);
-                    add(t);
-                    updateUI();
-                }
-            }
-
+        for(Spieler s: Start.getSession().getLoggedInspieler()){
+            JTextArea t = new JTextArea();
+            t.setText(s.getUsername());
+            t.setBorder(new LineBorder(Color.black,2));
+            t.setLocation(20,Start.getSession().getLoggedInspieler().indexOf(s)*40);
+            add(t);
+        }
     }
 
 }
+
