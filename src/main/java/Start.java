add1b39d384c61bba74e4074ac0d4b1a609cf093
SamiOsele <Samiosele23@gmail.com>
2022-03-17 14:17:25 +0100

    siu

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -3,11 +3,10 @@ public class Start {
     private static boolean debugMode = true;
     public static void main(String[] args) {
 //        if(args[0].equalsIgnoreCase("debug true")) debugMode = true;
-
+        MainFrame main= new MainFrame();
          dbv = new Datenbankverbindung();
         HandleCmdInput hci = new HandleCmdInput();
         hci.input();
-
     }
 
     public static Datenbankverbindung getDbv() {
