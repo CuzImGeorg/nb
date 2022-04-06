717cc3fddc7059c0b49ce4cfaf90d34a49496963
SamiOsele <Samiosele23@gmail.com>
2022-04-06 15:01:51 +0200

    siu

--- src/main/java/FrageAntwort.java
+++ src/main/java/FrageAntwort.java
@@ -68,6 +68,19 @@ public class FrageAntwort {
         return "SELECT * FROM FrageAntwort WHERE id = '" + id+ "'";
     }
 
+    public int getIDFrageAntwort(int Frageid, int Antwortid, int spielid, int rundeid){
+
+        try {
+            ResultSet rs = st.executeQuery(  "SELECT id FROM FrageAntwort WHERE Frageid=" + Frageid+ " AND Antwortid ="+Antwortid+" AND spielid ="+spielid+" AND rundeid = "+rundeid);
+
+
+            return rs.getInt("id");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+return 0;
+    }
+
     public FrageAntwort SetFullRecordAntwort(int id){
         try {
             ResultSet rs = st.executeQuery(getsiuu(id));
@@ -85,6 +98,7 @@ public class FrageAntwort {
         }
         return this;
     }
+
 
     static void newFrageAntwort(int Antwortid, int Frageid, int rundeid, int spielid){
 
